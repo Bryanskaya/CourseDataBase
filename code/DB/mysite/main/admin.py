300c2627bafb86204061d48462d64f89dc26762e
Bryanskaya <bryanskayakatya@yandex.ru>
2021-02-06 00:12:58 +0300

    SUCCESS: connected django & postgresql

--- code/DB/mysite/main/admin.py
+++ code/DB/mysite/main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Accounts
+
+admin.site.register(Accounts)
 
 # Register your models here.
--- code/DB/mysite/main/views.py
+++ code/DB/mysite/main/views.py
@@ -3,5 +3,6 @@ from .models import Accounts
 
 
 def home(request):
-    data = Accounts.objects.all()
+    data = Accounts.objects.all()[:3]
+    print("****", data)
     return render(request, 'main\index.html', {'title': 'Пробуем вывести', 'info': data})
