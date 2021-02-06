8d03b8228508f8b9bb2d8958de0ab90c1553f1bf
Bryanskaya <bryanskayakatya@yandex.ru>
2021-02-07 00:14:27 +0300

    ADD: 2 funcs

--- code/DB/mysite/main/models.py
+++ code/DB/mysite/main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db import connection
 
 
 class Accounts(models.Model):
@@ -10,3 +11,47 @@ class Accounts(models.Model):
 
     def __str__(self):
         return self.login
+
+
+class VouchersDetailed(models.Model):
+    id_voucher = models.IntegerField('Идентификатор', primary_key=True)
+    animal = models.TextField('Животное')
+    amount_animals = models.IntegerField('Количество животных')
+    duration_days = models.IntegerField('Количество дней')
+    id_sector = models.IntegerField('Идентификатор сектора')
+    price = models.DecimalField('Цена', max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return "%s" % self.id_voucher
+
+
+class SectorVouchers(models.Model):
+    ticket_num = models.IntegerField('Идетификатор охотника')
+    surname = models.CharField('Фамилия', max_length=30)
+    firstname = models.CharField('Имя', max_length=30)
+    patronymic = models.CharField('Отчество', max_length=30, default=None)
+    date_of_birth = models.DateField('Дата рождения', auto_now=False, auto_now_add=False)
+    mobile_phone = models.CharField('Мобильный телефон', max_length=30)
+    id_voucher = models.IntegerField('Идетификатор путёвки', primary_key=True)
+    animal = models.TextField('Животное')
+    amount_animals = models.IntegerField('Количество животных')
+    duration_days = models.IntegerField('Количество дней')
+    price = models.DecimalField('Цена', max_digits=5, decimal_places=2)
+
+
+# Получить все путёвки на конкретного пользователя
+def get_vouchers_by_id_hnt(id_hnt):
+    '''cursor = connection.cursor()
+    cursor.callproc("ShowVouchersByIdHnt", (str(id_hnt),))
+    res = cursor.fetchall()
+    cursor.close()
+    return res'''
+
+    res = VouchersDetailed.objects.raw('SELECT * FROM ShowVouchersByIdHnt(%s);', [id_hnt])
+    return res
+
+# Получить все путёвки в конкретном секторе
+def get_vouchers_by_id_sct(id_sct):
+    res = SectorVouchers.objects.raw('SELECT * FROM ShowVouchersByIdSct(%s);', [id_sct])
+    return res
+
--- code/DB/mysite/main/views.py
+++ code/DB/mysite/main/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from .models import Accounts
+# from .models import Accounts
+from .models import *
 
 
 def home(request):
-    data = Accounts.objects.all()[:3]
-    print("****", data)
+    #data = Accounts.objects.filter(login__exact='GorNa_03_08_1988')
+    data = get_vouchers_by_id_sct(55)
+    #print("+++++", data)
     return render(request, 'main\index.html', {'title': 'Пробуем вывести', 'info': data})
