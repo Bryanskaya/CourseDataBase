89d813712ad5d6ae2702e473df8470f6019c917d
Bryanskaya <bryanskayakatya@yandex.ru>
2021-04-18 08:34:28 +0300

    CREATED: several models for peewee

--- code/DB/pattern_repository/peewee_models.py
+++ code/DB/pattern_repository/peewee_models.py
@@ -1,18 +1,32 @@
 from peewee import *
 import inject
 
+from pattern_repository.repository import CurConnection
+
+
+def transf_to_objs(data, to_class):
+    arr = []
+
+    for temp in data.dicts():
+        arr.append(to_class(temp))
+
+    return arr
+
+
 class BaseModel(Model):
     class Meta:
-        database = inject.instance(////////)
+        database = inject.instance(CurConnection)
 
-class accounts(BaseModel):
+
+class Accounts(BaseModel):
     pass
 
-class hunters(BaseModel):
+
+class HunterModel(BaseModel):
     ticket_num = IntegerField(column_name='ticket_num', primary_key=True)
     surname = TextField(column_name='surname')
-    firstname = TextField(column_name='surname')
-    patronymic = TextField(column_name='surname')
+    firstname = TextField(column_name='firstname')
+    patronymic = TextField(column_name='patronymic')
     date_of_birth = DateTimeField(column_name='date_of_birth')
     sex = CharField(column_name='sex')
     residence = TextField(column_name='residence')
@@ -20,3 +34,15 @@ class hunters(BaseModel):
     email = TextField(column_name='email')
     login = TextField(column_name='login')
     #login = ForeignKeyField(accounts, column_name='login')
+
+    class Meta:
+        table_name = 'hunters'
+
+
+class HuntingGroundsModel(BaseModel):
+    id = IntegerField(column_name='id', primary_key=True)
+    ground_name = TextField(column_name='ground_name')
+    square = FloatField(column_name='square')
+
+    class Meta:
+        table_name = 'hunting_grounds'
