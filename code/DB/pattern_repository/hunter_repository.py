694499ee5072653f2e1f70b52ff65faf204b41d6
Bryanskaya <bryanskayakatya@yandex.ru>
2021-04-20 23:28:59 +0300

    MODIFIED: hunting_repository.py

--- code/DB/pattern_repository/hunter_repository.py
+++ code/DB/pattern_repository/hunter_repository.py
@@ -13,21 +13,13 @@ class PW_HunterRepository(HunterRepository):
     def __init__(self):
         pass
 
-    def create(self, obj):
-        #try:
-            HunterModel.create(ticket_num=obj.get_ticket_num(),
-                          surname=obj.get_surname(),
-                          firstname=obj.get_firstname(),
-                          patronymic=obj.get_patronymic(),
-                          date_of_birth=obj.get_date_of_birth(),
-                          sex=obj.get_sex(),
-                          residence=obj.get_residence(),
-                          mobile_phone=obj.get_mobile_phone(),
-                          email=obj.get_email(),
-                          login=obj.get_login())
-        #except:
-        #    return CreareBLObjectError
-
+    def create(self, obj: HunterModel):
+        # try:
+        HunterModel.create(ticket_num=obj.get_ticket_num(),
+                           residence=obj.get_residence(),
+                           login=obj.get_login())
+    # except:
+    #    return CreareBLObjectError
 
     def get(self):
         temp = HunterModel.select()
