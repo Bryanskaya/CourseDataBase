ee4e0ce8e042a50bb3a8bb8b2c79afcc98b7d6b6
Bryanskaya <bryanskayakatya@yandex.ru>
2021-04-20 23:21:51 +0300

    ADDED: BL object sectors

---
+++ code/DB/BL_objects/sectors.py
@@ -0,0 +1,18 @@
+class Sector(object):
+    id = None
+    square = None
+    id_husbandry = None
+
+    def __init__(self, data):  # **
+        if data is None:
+            return
+
+        self.id = data['id']
+        self.square = data['square']
+        self.id_husbandry = data['id_husbandry']
+
+    def get_id(self):           return self.id
+
+    def get_square(self):       return self.square
+
+    def get_id_husbandry(self): return self.id_husbandry
