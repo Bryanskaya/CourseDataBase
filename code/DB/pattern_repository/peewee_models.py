62c132b98d6e91f9b6c8b6fb8afd268bafefe604
Bryanskaya <bryanskayakatya@yandex.ru>
2021-04-20 23:31:05 +0300

    ADDED: some peewee models

--- code/DB/pattern_repository/peewee_models.py
+++ code/DB/pattern_repository/peewee_models.py
@@ -18,22 +18,27 @@ class BaseModel(Model):
         database = inject.instance(CurConnection)
 
 
-class Accounts(BaseModel):
-    pass
-
-
-class HunterModel(BaseModel):
-    ticket_num = IntegerField(column_name='ticket_num', primary_key=True)
+class AccountModel(BaseModel):
+    login = TextField(column_name='login', primary_key=True)
+    pswd = TextField(column_name='pswd')
     surname = TextField(column_name='surname')
     firstname = TextField(column_name='firstname')
     patronymic = TextField(column_name='patronymic')
     date_of_birth = DateTimeField(column_name='date_of_birth')
     sex = CharField(column_name='sex')
-    residence = TextField(column_name='residence')
     mobile_phone = TextField(column_name='mobile_phone')
     email = TextField(column_name='email')
-    login = TextField(column_name='login')
-    #login = ForeignKeyField(accounts, column_name='login')
+    type_role = TextField(column_name='type_role')
+
+    class Meta:
+        table_name = 'accounts'
+
+
+class HunterModel(BaseModel):
+    ticket_num = IntegerField(column_name='ticket_num', primary_key=True)
+    residence = TextField(column_name='residence')
+    #login = TextField(column_name='login')
+    login = ForeignKeyField(AccountModel, column_name='login')
 
     class Meta:
         table_name = 'hunters'
@@ -46,3 +51,17 @@ class HuntingGroundsModel(BaseModel):
 
     class Meta:
         table_name = 'hunting_grounds'
+
+
+class SectorModel(BaseModel):
+    id = IntegerField(column_name='id', primary_key=True)
+    square = FloatField(column_name='square')
+    id_husbandry = ForeignKeyField(HuntingGroundsModel, column_name='id_husbandry')
+
+    class Meta:
+        table_name = 'sectors'
+
+
+
+
+
