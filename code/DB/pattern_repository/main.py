fd548efc5aaecc7bdcc57fbc91c9792fc91dbb6a
Bryanskaya <bryanskayakatya@yandex.ru>
2021-04-20 23:33:05 +0300

    ADDED: some tests

--- code/DB/pattern_repository/main.py
+++ code/DB/pattern_repository/main.py
@@ -7,23 +7,41 @@ from BL_objects import *
 if __name__ == '__main__':
     # hunters = inject.instance(HunterRepository)
     # hunters.create(Hunter({'ticket_num': '0000000000',
-    #                       'surname': 'Иванов',
-    #                       'firstname': 'Иван',
-    #                       'patronymic': 'Иванович',
-    #                       'date_of_birth': '01.01.2000',
-    #                       'sex': 'м',
     #                       'residence': 'г. Москва',
-    #                       'mobile_phone': '8 910 000 00 00',
-    #                       'email': 'smth@main.ru',
-    #                       'login': 'IvaIv_01_01_2000'}))
-    '''arr = hunters.get()
-    print(arr)'''
+    #                       'login': 'login_test1'}))
+    # arr = hunters.get()
+    # print(arr)
 
-    hunting_gr = inject.instance(HuntingGroundsRepository)
+    # hunting_gr = inject.instance(HuntingGroundsRepository)
+    #
+    # hunting_gr.create(HuntingGrounds({'id': 2,
+    #                                   'ground_name': 'Тест2',
+    #                                   'square': '2'}))
+    #
+    # arr = hunting_gr.get()
+    # print(arr)
 
-    hunting_gr.create(HuntingGrounds({'id': 2,
-                                      'ground_name': 'Тест2',
-                                      'square': '2'}))
+    # sector = inject.instance(SectorsRepository)
+    # sector.create(Sector({'id': 101,
+    #                       'square': 1000000,
+    #                       'id_husbandry': 1}))
+    # arr = sector.get()
+    # print(arr)
 
-    arr = hunting_gr.get()
-    print(arr)
+    # account = inject.instance(AccountsRepository)
+    # account.create(Account({'login': 'login_test1',
+    #                        'pswd': 'pswd_test1',
+    #                        'surname': 'Иванов',
+    #                        'firstname': 'Иван',
+    #                        'patronymic': 'Иванов',
+    #                        'date_of_birth': '2000-01-01',
+    #                        'sex': 'м',
+    #                        'mobile_phone': '89000000000',
+    #                        'email': 'test_email1@smth.ru',
+    #                        'type_role': 'охотник'}))
+    # arr = account.get()
+    # print(arr)
+
+
+
+
