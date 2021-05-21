6b176083928673a3c8e33bfb12f9a7ed13a190dc
Bryanskaya <bryanskayakatya@yandex.ru>
2021-05-21 13:17:20 +0300

    ADD: registatration page

---
+++ code/DB/mysite/templates/static/js/main.js
@@ -0,0 +1,23 @@
+function HiddenFields(){
+    var elem = document.getElementById("role").value;
+    if (elem === 'hunter')
+    {
+        document.getElementById("add-info").style.display = "flex";
+        document.getElementById("label-info").style.display = "block";
+        document.getElementById("ticket_num").style.display = "block";
+        document.getElementById("address").style.display = "block";
+
+        document.getElementById("ticket_num").required = true;
+        document.getElementById("address").required = true;
+    }
+    else
+    {
+        document.getElementById("add-info").style.display = "none";
+
+        document.getElementById("ticket_num").required = false;
+        document.getElementById("address").required = false;
+
+    }
+}
+
+
