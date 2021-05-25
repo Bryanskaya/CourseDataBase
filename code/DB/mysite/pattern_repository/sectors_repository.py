c8b0331981c3a664db5b2391091042a63082fc44
Bryanskaya <bryanskayakatya@yandex.ru>
2021-05-25 09:31:36 +0300

    CORRECTED: fields in sectors

--- code/DB/mysite/pattern_repository/sectors_repository.py
+++ code/DB/mysite/pattern_repository/sectors_repository.py
@@ -1,26 +1,36 @@
 from pattern_repository.repository import *
 from pattern_repository.peewee_models import *
 from BL_objects.sectors import *
+from errors.err_sector import *
 
 
 class SectorsRepository(Repository):
-    def __init__(self):
-        pass
+    def create(self, obj: Sector):
+        raise NotImplementedError
+
+    def delete(self, obj: Sector):
+        raise NotImplementedError
+
+    def get_all(self) -> [Sector]:
+        raise NotImplementedError
+
+    def get_ids(self, id_hunting_ground):
+        raise NotImplementedError
 
 
 class PW_SectorsRepository(SectorsRepository):
-    def __init__(self):
-        pass
-
     def create(self, obj: Sector):
-        # try:
-        SectorModel.create(id=obj.get_id(),
-                           id_husbandry=obj.get_id_husbandry())
-
-    # except:
-    #    return CreareBLObjectError
+        try:
+            SectorModel.create(id=obj.get_id(),
+                               id_husbandry=obj.get_id_husbandry())
+        except:
+           raise CreateBLObjectSectorErr()
 
     def get(self):
         temp = SectorModel.select()
-
         return transf_to_objs(temp, Sector)
+
+    def get_ids(self, id_hunting_ground):
+        temp = SectorModel.select().where(SectorModel.id_husbandry == id_hunting_ground)
+        sectors_set = transf_to_objs(temp, Sector)
+        return [x.get_id() for x in sectors_set]
