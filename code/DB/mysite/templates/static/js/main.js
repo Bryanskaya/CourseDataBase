cf3dc345c485212d4fd9871ced46e030302ff5b4
Bryanskaya <bryanskayakatya@yandex.ru>
2021-05-25 09:28:18 +0300

    CORRECTED: style and form of registration

--- code/DB/mysite/templates/static/js/main.js
+++ code/DB/mysite/templates/static/js/main.js
@@ -16,8 +16,68 @@ function HiddenFields(){
 
         document.getElementById("ticket_num").required = false;
         document.getElementById("address").required = false;
+    }
 
+    if (elem === 'huntsman' || elem === 'егерь')
+    {
+        document.getElementById("add-work").style.display = "flex";
+        document.getElementById("label-work").style.display = "block";
+        document.getElementById("hunting_grounds").style.display = "block";
+        document.getElementById("sectors").style.display = "block";
+
+        document.getElementById("hunting_grounds").required = true;
+        document.getElementById("sectors").required = true;
+    }
+    else
+    {
+        document.getElementById("add-work").style.display = "none";
+
+        document.getElementById("hunting_grounds").required = false;
+        document.getElementById("sectors").required = false;
     }
 }
 
+//function GetSectors() {
+//    const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
+//
+//    var elem = document.getElementById("hunting_grounds").value;
+//    console.log(elem);
+//
+//    const request = new XMLHttpRequest();
+//    const url = "get_sectors/";
+//    const params = "id=" + elem;
+//
+//    request.responseType =	"json";
+//    request.open("POST", url, true);
+//    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+//    request.setRequestHeader("X-CSRFToken", csrftoken);
+//
+//    request.addEventListener("readystatechange", () => {
+//
+//    if (request.readyState === 4 && request.status === 200) {
+//        let obj = request.response;
+//
+//        console.log(obj.id);
+//
+//        var docfrag = document.createDocumentFragment();
+//
+//        for (var i = 0; i < obj.id.length; i++)
+//        {
+//             docfrag.appendChild(new Option(obj.id[i], obj.id[i]));
+//        }
+//
+//        var select = document.getElementById("sectors");
+//        select.innerHTML = "";
+//
+//        if (obj.id.length === 0) {
+//            select.appendChild(new Option("Нет доступных", null));
+//        }
+//        else {
+//            select.appendChild(docfrag);
+//        }
+//	}
+//});
+//    console.log(document.getElementById("sectors"));
+//    request.send(params);
+//}
 
