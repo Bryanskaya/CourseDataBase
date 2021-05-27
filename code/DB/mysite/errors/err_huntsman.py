997f8c7162a6b10995abb23b6d0a7190bfa10773
Bryanskaya <bryanskayakatya@yandex.ru>
2021-05-28 01:27:14 +0300

    ADDED: update() + some others

--- code/DB/mysite/errors/err_huntsman.py
+++ code/DB/mysite/errors/err_huntsman.py
@@ -1,1 +1,3 @@
 class CreateBLObjectHuntsmanErr(BaseException): pass
+class IdHuntsmanNotExists(BaseException):    pass
+class UpdateHuntsmanErr(BaseException):         pass
--- code/DB/mysite/pattern_repository/huntsman_repository.py
+++ code/DB/mysite/pattern_repository/huntsman_repository.py
@@ -1,11 +1,24 @@
 from pattern_repository.repository import *
 from pattern_repository.peewee_models import *
 from BL_objects.huntsman import *
+from errors.err_huntsman import *
 
 
 class HuntsmanRepository(Repository):
-    def __init__(self):
-        pass
+    def create(self, obj: HuntsmanModel):
+        raise NotImplementedError
+
+    def delete(self, obj: Huntsman):
+        raise NotImplementedError
+
+    def update(self, obj_old: Huntsman, obj_upd: Huntsman):
+        raise NotImplementedError
+
+    def get_all(self) -> [Huntsman]:
+        raise NotImplementedError
+
+    def get_by_id(self, id) -> Huntsman:
+        raise NotImplementedError
 
 
 class PW_HuntsmanRepository(HuntsmanRepository):
@@ -13,13 +26,34 @@ class PW_HuntsmanRepository(HuntsmanRepository):
         pass
 
     def create(self, obj: HuntsmanModel):
-        # try:
-        HuntsmanModel.create(id=obj.get_id(),
-                             login=obj.get_login())
-    # except:
-    #    return CreareBLObjectError
+        try:
+            HuntsmanModel.create(id=obj.get_id(),
+                                 login=obj.get_login())
+        except:
+           return CreateBLObjectHuntsmanErr()
 
-    def get(self):
+    def delete(self, obj: Huntsman):
+        temp = HuntsmanModel.delete().where(HuntsmanModel.id == obj.id)
+        temp.execute()
+
+    # def update(self, obj_old: Huntsman, obj_upd: Huntsman):
+    #     if self.get_by_id(obj_old.id) is None:
+    #         raise IdHuntsmanNotExists
+    #
+    #     temp = self.model.update(obj_upd.get_dict()).where(HuntsmanModel.id == obj_upd.id)
+    #     try:
+    #         temp.execute()
+    #     except:
+    #         raise UpdateHuntsmanErr
+
+    def get_all(self) -> [Huntsman]:
         temp = HuntsmanModel.select()
-
         return transf_to_objs(temp, Huntsman)
+
+    def get_by_id(self, id) -> Huntsman:
+        temp = HuntsmanModel.select().where(HuntsmanModel.id == id)
+        huntsman_set = transf_to_objs(temp, Huntsman)
+
+        if len(huntsman_set):
+            return huntsman_set[0]
+        return None
