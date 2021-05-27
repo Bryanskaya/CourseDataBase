00c4f36cb2ac0a193a32903021cee4bc645ba553
Bryanskaya <bryanskayakatya@yandex.ru>
2021-05-28 00:37:39 +0300

    ADDED: error

--- code/DB/mysite/errors/err_account.py
+++ code/DB/mysite/errors/err_account.py
@@ -2,4 +2,5 @@ class AuthorisedErr(BaseException): pass
 class WrongRoleErr(BaseException): pass
 
 class CreateBLObjectAccountErr(BaseException): pass
-
+class LoginNotExists(BaseException): pass
+class UpdateAccountErr(BaseException): pass
