0cd5d69b7c6d5d3edab8389e9739d2545b750897
Bryanskaya <bryanskayakatya@yandex.ru>
2021-05-28 18:41:39 +0300

    CHANGED: base class

--- code/DB/mysite/BL_rules/huntsman_rules.py
+++ code/DB/mysite/BL_rules/huntsman_rules.py
@@ -1,12 +1,12 @@
 from inject_config import *
 from BL_objects.huntsman import *
 from errors.err_huntsman import *
+from BL_rules.base_rules import *
 
 
-class HuntsmanRules(object):
-    @staticmethod
-    def register(obj: Huntsman):
-        huntsman_set = inject.instance(HuntsmanRepository)
+class HuntsmanRules(BaseRules):
+    def register(self, obj: Huntsman):
+        huntsman_set = inject.instance(HuntsmanRepository)(self.connection)
         try:
             huntsman_set.create(obj)
         except CreateBLObjectHuntsmanErr:
