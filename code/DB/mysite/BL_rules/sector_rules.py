dfc35def6236674a7eb62e9118acc2c81ea341cc
Bryanskaya <bryanskayakatya@yandex.ru>
2021-05-31 01:39:12 +0300

    ADDED: sectors rules

--- code/DB/mysite/BL_rules/sector_rules.py
+++ code/DB/mysite/BL_rules/sector_rules.py
@@ -5,3 +5,9 @@ class SectorRules(BaseRules):
     def get_ids(self, id_hunting_ground):
         sectors_set = inject.instance(SectorsRepository)(self.connection)
         return sectors_set.get_ids(id_hunting_ground)
+
+    def get_id_husbandry(self, id):
+        sectors_set = inject.instance(SectorsRepository)(self.connection)
+        return sectors_set.get_by_id(id).get_dict()
+
+
