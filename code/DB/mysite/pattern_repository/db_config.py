78da60f0e67d5d21515f77f2f2fadc28f03748ec
Bryanskaya <bryanskayakatya@yandex.ru>
2021-05-30 17:16:18 +0300

    CORRECTED: config document

--- code/DB/mysite/pattern_repository/db_config.py
+++ code/DB/mysite/pattern_repository/db_config.py
@@ -2,9 +2,9 @@ from peewee import *
 import inject
 
 def get_config_params(to_class, config):
-    db_name = config.get_item('db_name')
-    user = config.get_item('db_user')
-    pwd = config.get_item('db_password')
-    host = config.get_item('db_host')
+    db_name = config['db_name']
+    user = config['db_user']
+    pwd = config['db_password']
+    host = config['db_host']
 
     return to_class(db_name, user=user, password=pwd, host=host, autorollback=True)
