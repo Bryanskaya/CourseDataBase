8dd38597df0009817d65d1c3948d30b1ad76efe4
Bryanskaya <bryanskayakatya@yandex.ru>
2021-05-31 01:38:29 +0300

    CORRECTED: base rules

--- code/DB/mysite/BL_rules/base_rules.py
+++ code/DB/mysite/BL_rules/base_rules.py
@@ -4,7 +4,11 @@ from errors.err_account import *
 class BaseRules(object):
     connection = None
 
-    def __init__(self, role):
+    def __init__(self, role, connection=None):
+        if connection is not None:
+            self.connection = connection
+            return
+
         self.connection = inject.instance(CurConnection) #TODO change to role connection
 
         if role == 'admin' or role == 'админ':
