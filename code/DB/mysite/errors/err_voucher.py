e2196f123191ecbb1e77acf49632f9fd2bafa724
Bryanskaya <bryanskayakatya@yandex.ru>
2021-06-01 23:16:11 +0300

    ADDED: new error

--- code/DB/mysite/errors/err_voucher.py
+++ code/DB/mysite/errors/err_voucher.py
@@ -1,3 +1,4 @@
 class CreateBLObjectVoucherErr(BaseException):   pass
 class IdVoucherNotExists(BaseException):      pass
 class UpdateVoucherErr(BaseException):           pass
+class WrongNumAnimals(BaseException):           pass
