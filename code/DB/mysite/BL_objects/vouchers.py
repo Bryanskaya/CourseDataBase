35ef4f71966688df576fcd21b842974d4eb30241
Bryanskaya <bryanskayakatya@yandex.ru>
2021-06-01 23:14:45 +0300

    ADDED: new methods for vouchers

--- code/DB/mysite/BL_objects/vouchers.py
+++ code/DB/mysite/BL_objects/vouchers.py
@@ -1,29 +1,30 @@
 class Voucher(object):
     id = None
-    duration_days = None
     amount_animals = None
     price = None
     id_hunter = None
     id_pricelist = None
+    status = None
 
     def __init__(self, data):  # **
         if data is None:
             return
 
-        self.id = data['id']
-        self.duration_days = data['duration_days']
+        if 'id' in data.keys():
+            self.id = data['id']
         self.amount_animals = data['amount_animals']
         self.price = data['price']
         self.id_hunter = data['id_hunter']
         self.id_pricelist = data['id_pricelist']
+        self.status = data['status']
 
     def get_dict(self) -> dict:
         return {'id': self.id,
-                'duration_days': self.duration_days,
                 'amount_animals': self.amount_animals,
                 'price': self.price,
                 'id_hunter': self.id_hunter,
-                'id_pricelist': self.id_pricelist}
+                'id_pricelist': self.id_pricelist,
+                'status': self.status}
 
     def __ne__(self, other):
         return self.get_dict() != other.get_dict()
@@ -32,16 +33,16 @@ class Voucher(object):
         return self.get_dict() == other.get_dict()
 
     def get_id(self):               return self.id
-    def get_duration_days(self):    return self.duration_days
     def get_amount_animals(self):   return self.amount_animals
     def get_price(self):            return self.price
     def get_id_hunter(self):        return self.id_hunter
     def get_id_pricelist(self):     return self.id_pricelist
+    def get_status(self):           return self.status
 
     def set_id(self, id):               self.id = id
-    def set_duration_days(self, num):   self.duration_days = num
     def set_amount_animals(self, num):  self.amount_animals = num
     def set_price(self, price):         self.price = price
     def set_id_hunter(self, id):        self.id_hunter = id
     def set_id_pricelist(self, id):     self.id_pricelist = id
+    def set_status(self, status):       self.status = status
 
--- code/DB/mysite/BL_rules/voucher_rules.py
+++ code/DB/mysite/BL_rules/voucher_rules.py
@@ -1,7 +1,86 @@
 from BL_objects.vouchers import *
 
 from BL_rules.base_rules import *
+from BL_rules.pricelist_rules import *
+from BL_rules.hunter_rules import *
+from errors.err_voucher import *
 
 
 class VoucherRules(BaseRules):
-    pass
+    def create(self, data):
+        obj = self.build_obj(data)
+
+        vouchers_set = inject.instance(VoucherRepository)(self.connection)
+        voucher = Voucher(obj)
+        try:
+            vouchers_set.create(voucher)
+        except CreateBLObjectVoucherErr:
+            voucher = None
+
+        return voucher
+
+    def build_obj(self, data):
+        list_rules = PriceListRules('', connection=self.connection)
+
+        if data['num'] == 0:
+            raise WrongNumAnimals()
+
+        obj = list_rules.get_by_id(data['id'])
+
+        obj['amount_animals'] = data['num']
+        obj['price'] = obj['price'] * data['num']
+
+        hunter_rules = HunterRules('', connection=self.connection)
+        hunter = hunter_rules.get_by_login(data['login']).get_dict()
+
+        obj['id_hunter'] = hunter['ticket_num']
+        obj['id_pricelist'] = obj['id']
+        del obj['id']
+        obj['status'] = False
+
+        return obj
+
+    def get_sorted(self, data):
+        return sorted(data, key=lambda x: (x['ground_name'], x['id_sector'],
+                                           x['animal'], x['amount_animals']))
+
+    def get_by_login(self, login):
+        hunter_rules = HunterRules('', connection=self.connection)
+        vouchers_set = inject.instance(VoucherRepository)(self.connection)
+
+        hunter = hunter_rules.get_by_login(login).get_dict()
+        vouchers = vouchers_set.get_by_id_hunter(hunter['ticket_num'])
+
+        res_true = []
+        res_false = []
+        for voucher in vouchers:
+            temp = self.build_info(voucher)
+            if temp['status']:
+                res_true.append(temp)
+            else:
+                res_false.append(temp)
+
+        res_true = self.get_sorted(res_true)
+        res_false = self.get_sorted(res_false)
+
+        return res_true, res_false
+
+    def build_info(self, data: Voucher):
+        list_rules = PriceListRules('', connection=self.connection)
+        sector_rules = SectorRules('', connection=self.connection)
+        ground_rules = HuntingGroundsRules('', connection=self.connection)
+
+        data = data.get_dict()
+        item = list_rules.get_by_id(data['id_pricelist'])
+
+        data['animal'] = item['animal']
+        data['id_sector'] = item['id_sector']
+
+        sector = sector_rules.get_id_husbandry(data['id_sector'])
+        ground = ground_rules.get_by_id(sector['id_husbandry'])
+
+        data['ground_name'] = ground['ground_name']
+
+        return data
+
+
