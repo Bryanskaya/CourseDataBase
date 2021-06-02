a0877b09c466bbc5cb5cceb2169b3848afa31572
Bryanskaya <bryanskayakatya@yandex.ru>
2021-06-02 11:46:25 +0300

    ADDED: new permission

--- code/SQL_files/hunter_permissions.sql
+++ code/SQL_files/hunter_permissions.sql
@@ -3,4 +3,6 @@ GRANT SELECT ON accounts TO hunter;
 GRANT SELECT ON sectors TO hunter;
 GRANT SELECT ON hunting_grounds TO hunter;
 GRANT SELECT ON hunters TO hunter;
+GRANT SELECT ON vouchers TO hunter;
+GRANT ALL PRIVILEGES ON SEQUENCE vouchers_id_seq TO hunter;
 GRANT INSERT VALUES('id', 'amount_animals', 'price', 'id_hunter', 'id_pricelist', 'status') ON vouchers TO hunter;
