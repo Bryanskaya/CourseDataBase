081420e6212fe02a61c7ea208b9059862d04c7d2
Bryanskaya <bryanskayakatya@yandex.ru>
2021-06-02 18:37:34 +0300

    ADDED: conntacts of huntsmen

--- code/DB/mysite/BL_rules/huntsman_rules.py
+++ code/DB/mysite/BL_rules/huntsman_rules.py
@@ -17,3 +17,44 @@ class HuntsmanRules(BaseRules):
     def get_by_login(self, login):
         huntsman_set = inject.instance(HuntsmanRepository)(self.connection)
         return huntsman_set.get_by_login(login)
+
+    def get_all(self):
+        huntsman_set = inject.instance(HuntsmanRepository)(self.connection)
+        huntsmen = huntsman_set.get_all()
+
+        for i in range(len(huntsmen)):
+            huntsmen[i] = huntsmen[i].get_dict()
+
+        return huntsmen
+
+    def get_all_detailed(self):
+        huntsman_set = inject.instance(HuntsmanRepository)(self.connection)
+        huntsmen = huntsman_set.get_all()
+
+        for i in range(len(huntsmen)):
+            pers = huntsmen[i].get_dict()
+
+            accounts_set = inject.instance(AccountsRepository)(self.connection)
+            account = accounts_set.get_by_login(pers['login']).get_dict()
+
+            pers['id_sector'] = pers['id']
+            del pers['id']
+            pers['full_name'] = account['surname'] + ' ' + account['firstname'] + \
+                ' ' + account['patronymic']
+            pers['mobile_phone'] = account['mobile_phone']
+            pers['email'] = account['email']
+
+            sectors_set = inject.instance(SectorsRepository)(self.connection)
+            sector = sectors_set.get_by_id(pers['id_sector']).get_dict()
+
+            grounds_set = inject.instance(HuntingGroundsRepository)(self.connection)
+            ground = grounds_set.get_by_id(sector['id_husbandry']).get_dict()
+
+            pers['ground_name'] = ground['ground_name']
+
+            huntsmen[i] = pers
+
+        return huntsmen
+
+
+
