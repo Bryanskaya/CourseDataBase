6cab72782419593300ec5a64c2a1885c8bf65045
Bryanskaya <bryanskayakatya@yandex.ru>
2021-06-02 18:44:21 +0300

    ADDED: new permissions

--- code/SQL_files/hunter_permissions.sql
+++ code/SQL_files/hunter_permissions.sql
@@ -5,4 +5,9 @@ GRANT SELECT ON hunting_grounds TO hunter;
 GRANT SELECT ON hunters TO hunter;
 GRANT SELECT ON vouchers TO hunter;
 GRANT ALL PRIVILEGES ON SEQUENCE vouchers_id_seq TO hunter;
-GRANT INSERT VALUES('id', 'amount_animals', 'price', 'id_hunter', 'id_pricelist', 'status') ON vouchers TO hunter;
+GRANT INSERT ON vouchers TO hunter;
+GRANT DELETE ON vouchers TO hunter;
+GRANT INSERT VALUES('id', 'amount_animals', 'price', 'id_hunter', 'id_pricelist', 'status') INTO vouchers TO hunter;
+
+
+INSERT INTO vouchers(amount_animals, price, id_hunter, id_pricelist, status) VALUES('44', '44', '22222222', 20, null)
--- code/SQL_files/huntsman_permissions.sql
+++ code/SQL_files/huntsman_permissions.sql
@@ -1,2 +1,3 @@
 GRANT SELECT ON price_list TO huntsman;
 GRANT SELECT ON accounts TO huntsman;
+GRANT SELECT ON huntsmen TO huntsman;
