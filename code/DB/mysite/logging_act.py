b8f627ff77fe88b79922477eb304db97e6353474
Bryanskaya <bryanskayakatya@yandex.ru>
2021-06-05 01:37:14 +0300

    ADDED: option try/except

--- code/DB/mysite/logging_act.py
+++ code/DB/mysite/logging_act.py
@@ -18,9 +18,7 @@ def do_log(f, request, **kwargs):
                      login,
                      request.META['PATH_INFO'])
 
-    # try:
-    #     return f(request, **kwargs)
-    # except BaseException as e:
-    #     logging.exception(">>>ERROR: in %s (situated in) - %s", f.__name__, e.__class__, str(e))
-
-    return f(request, **kwargs)
+    try:
+        return f(request, **kwargs)
+    except BaseException as e:
+        logging.exception(">>>ERROR: in %s (situated in) - %s", f.__name__, e.__class__, str(e))
