1ed2d24eee13c033db3483e2ef8954372a2ff6d7
Bryanskaya <bryanskayakatya@yandex.ru>
2021-06-04 15:05:17 +0300

    ADDED: new errors

--- code/DB/mysite/errors/err_voucher.py
+++ code/DB/mysite/errors/err_voucher.py
@@ -3,3 +3,5 @@ class IdVoucherNotExists(BaseException):      pass
 class UpdateVoucherErr(BaseException):           pass
 class WrongNumAnimals(BaseException):           pass
 class DeleteVoucherErr(BaseException):           pass
+class VoucherNotExists(BaseException):          pass
+class AcceptVoucherErr(BaseException):          pass
